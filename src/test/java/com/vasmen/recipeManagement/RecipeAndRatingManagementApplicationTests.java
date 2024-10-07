6ff95b33d04bb0798244607df1000708f75517e9
Duxs96 <ericvasquez07@gmail.com>
2024-10-07 16:00:24 -0300

    Initial Commit For Spring Boot Project

---
+++ src/test/java/com/vasmen/recipeManagement/RecipeAndRatingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasmen.recipeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAndRatingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
