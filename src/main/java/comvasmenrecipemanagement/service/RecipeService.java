86f700dec84f15efd0c55d7ef88440b4c0da96c2
Duxs96 <ericvasquez07@gmail.com>
2024-10-08 17:10:56 -0300

    feat: Se agrega clase RecipeService

---
+++ src/main/java/comvasmenrecipemanagement/service/RecipeService.java
@@ -0,0 +1,121 @@
+package comvasmenrecipemanagement.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import comvasmenrecipemanagement.dto.RecipeCreateDTO;
+import comvasmenrecipemanagement.dto.RecipeDTO;
+import comvasmenrecipemanagement.dto.RecipeUpdateDTO;
+import comvasmenrecipemanagement.entity.Recipe;
+import comvasmenrecipemanagement.repository.RecipeRepository;
+
+@Service
+public class RecipeService {
+
+    private final RecipeRepository _recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository){
+        _recipeRepository = recipeRepository;
+    }
+
+    public List<RecipeDTO> getRecipes() {
+        List<Recipe> recipes = _recipeRepository.findAll();
+        List<RecipeDTO> recipesDTO = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            recipesDTO.add(mappingToDTO(recipe));
+        }
+        return recipesDTO;
+    }
+
+    public RecipeDTO getRecipeById(int id){
+        Optional<Recipe> recipe = _recipeRepository.findById(id);
+        RecipeDTO recipeDTO = mappingToDTO(recipe.get());
+        return recipeDTO;
+    }
+
+    public RecipeDTO saveRecipe(RecipeCreateDTO recipeDTO){
+        Recipe recipe = mappingToEntity(recipeDTO);
+        _recipeRepository.save(recipe);
+        RecipeDTO recipeDTOCreated = mappingToDTO(recipe);
+        return recipeDTOCreated;
+    }
+
+    public RecipeDTO updateRecipe(int id, RecipeUpdateDTO recipeUpdateDTO){
+        Optional<Recipe> recipe = _recipeRepository.findById(id);
+        if(recipe.isPresent()){
+            _recipeRepository.save(mappingDTOToEntity(recipe.get(), recipeUpdateDTO));
+            return mappingToDTO(recipe.get());
+        }
+        return null;
+    }
+
+    public Boolean deleteRecipe(int id){
+        Boolean exists = _recipeRepository.existsById(id);
+        if(!exists){ return null; }
+        _recipeRepository.deleteById(id);
+        return true;
+    }
+
+    public RecipeDTO updateVotes(int id, boolean vote){
+        Optional<Recipe> recipe = _recipeRepository.findById(id);
+        if(recipe.isPresent()){
+            Recipe recipeVotesUpdated = updateVotes(recipe.get(), vote);
+            _recipeRepository.save(recipeVotesUpdated);
+            return mappingToDTO(recipeVotesUpdated);
+        }
+        return null;
+    }
+
+    private Recipe updateVotes(Recipe recipe, boolean vote){
+        if(vote){
+            recipe.setVotes(recipe.getVotes() + 1);
+        }else{
+            recipe.setVotes(recipe.getVotes() - 2);
+        }
+        return recipe;
+    }
+
+    private Recipe mappingDTOToEntity(Recipe recipe, RecipeUpdateDTO recipeDTO){
+        recipe.setId(recipeDTO.getId());
+        recipe.setName(recipeDTO.getName());
+        recipe.setDescription(recipeDTO.getDescription());
+        recipe.setIngredients(recipeDTO.getIngredients());
+        recipe.setInstructions(recipeDTO.getInstructions());
+        recipe.setPreparationTime(recipeDTO.getPreparationTime());
+        recipe.setDifficulty(recipeDTO.getDifficulty());
+        recipe.setParticipant(recipeDTO.getParticipant());
+        // recipe.setVotes(recipeDTO.getVotes());
+        return recipe;
+    }
+
+    private RecipeDTO mappingToDTO(Recipe recipe){
+        RecipeDTO recipeDTO = new RecipeDTO();
+        if(recipe == null){return recipeDTO;}
+        recipeDTO.setId(recipe.getId());
+        recipeDTO.setName(recipe.getName());
+        recipeDTO.setDescription(recipe.getDescription());
+        recipeDTO.setIngredients(recipe.getIngredients());
+        recipeDTO.setInstructions(recipe.getInstructions());
+        recipeDTO.setPreparationTime(recipe.getPreparationTime());
+        recipeDTO.setDifficulty(recipe.getDifficulty());
+        recipeDTO.setParticipant(recipe.getParticipant());
+        recipeDTO.setVotes(recipe.getVotes());
+        return recipeDTO;
+    }
+
+    private Recipe mappingToEntity(RecipeCreateDTO recipecreateDTO){
+        Recipe recipe = new Recipe();
+        recipe.setName(recipecreateDTO.getName());
+        recipe.setDescription(recipecreateDTO.getDescription());
+        recipe.setIngredients(recipecreateDTO.getIngredients());
+        recipe.setInstructions(recipecreateDTO.getInstructions());
+        recipe.setPreparationTime(recipecreateDTO.getPreparationTime());
+        recipe.setDifficulty(recipecreateDTO.getDifficulty());
+        recipe.setParticipant(recipecreateDTO.getParticipant());
+        return recipe;
+    }
+
+}
