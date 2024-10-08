5ec2cd9c407be4be8f414761583b60aae2883bcf
Duxs96 <ericvasquez07@gmail.com>
2024-10-08 19:59:22 -0300

    feat: Se agrega controlador por defecto para cuando se quiera acceder a la / del proyecto, redirige a Swagger

---
+++ src/main/java/comvasmenrecipemanagement/controller/DefaultController.java
@@ -0,0 +1,20 @@
+package comvasmenrecipemanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.io.IOException;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/")
+@Tag(name="Redirigir a Swagger por defecto")
+public class DefaultController {
+
+    @GetMapping()
+    public void redirectToSwagger(HttpServletResponse response) throws IOException, java.io.IOException {
+        response.sendRedirect("/swagger-ui/index.html");
+    }
+}
