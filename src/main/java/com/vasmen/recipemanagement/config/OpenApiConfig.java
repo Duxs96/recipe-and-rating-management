1109940a22dab2d24747a176a1411fb66d46ecfc
Duxs96 <ericvasquez07@gmail.com>
2024-10-08 21:31:12 -0300

    refactor: Corrección de nombre de package

--- src/main/java/com/vasmen/recipemanagement/config/OpenApiConfig.java
+++ src/main/java/com/vasmen/recipemanagement/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package comvasmenrecipemanagement.config;
+package com.vasmen.recipemanagement.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
--- src/main/java/com/vasmen/recipemanagement/controller/DefaultController.java
+++ src/main/java/com/vasmen/recipemanagement/controller/DefaultController.java
@@ -1,4 +1,4 @@
-package comvasmenrecipemanagement.controller;
+package com.vasmen.recipemanagement.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
